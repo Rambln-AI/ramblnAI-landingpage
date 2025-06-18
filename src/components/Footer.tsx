43d1668fad8083c195d26b6be9a87d535e4fb91d
Junhyuk Hur <jun002chung@gmail.com>
2025-06-18 11:57:40 -0500

    Update email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
                 <Github className="w-5 h-5" />
               </a>
               <a 
-                href="mailto:jun18shadow@gmail.com" 
+                href="mailto:management.rambln.ai@gmail.com" 
                 className="w-10 h-10 bg-white/10 rounded-lg flex items-center justify-center hover:bg-[#B3A369] transition-all duration-300 hover:scale-110"
               >
                 <Mail className="w-5 h-5" />
